645cc04c84dab187a04fcc902711dd608630cf04
Ravi3599 <94590162+Ravi3599@users.noreply.github.com>
2021-12-10 14:58:49 +0530

    UPDATE: uc1

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,7 +3,7 @@ public class SnakeLadder {
 	public static void main(String[] args){
 		int start = 0;
 		System.out.println("Starting position of player is: " + start);
-		int roll = (int) (Math.floor(Math.random() * 10) % 2 + 1);
+		int roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
 		System.out.println(roll);
 	}
 }
