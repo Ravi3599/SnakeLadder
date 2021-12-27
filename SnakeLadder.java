fffbad15097a08014598e6bbb269d27ddf6897f7
Ravi3599 <94590162+Ravi3599@users.noreply.github.com>
2021-12-27 10:43:20 +0530

    MODIFIED: some space changes

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -6,116 +6,105 @@ public class SnakeLadder {
 
 
    public static void main(String[] args){
-      int pos = 0, pos1 = 0, cntr1 = 0, cntr2 = 0, cntr3 = 0, cntr4 = 0, c1 = 0, c2 = 0, c3 = 0, c4 = 0;
-		//int con = 1;
+      int positionPlayer1 = 0, positionPlayer2 = 0, cntForBets = 0, cntForLadder = 0, cntForSnake = 0, cntForNoPlay = 0, cntForBets2 = 0, cntForLadder2 = 0, cntForSnake2 = 0, cntForNoPlay2 = 0;
       System.out.println("Starting position of player is: " + START);
-      while(pos < 100 && pos1 < 100){
-		int con = 1;
-a:		while(con == 1){
+      while(positionPlayer1 < 100 && positionPlayer2 < 100){
+		int conition1 = 1;
+a:		while(condition1 == 1){
 		System.out.println("PLAYER 1");
-      int roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-		cntr1++;
-      System.out.println("Number on rolled dice is : " +roll);
-      con = (int) (Math.floor(Math.random() * 10) % 3);
-      System.out.println("Condition: " +con);
-      switch (con){
-            case NO_PLAY:
-						System.out.println("NO PLAY");
-                  System.out.println("New Position is: " + pos);
-						cntr4++;
-                  break;
-            case LADDER:
-						System.out.println("Got LADDER");
-                  pos = pos + roll;
-                  if(pos <= 100)
-                  System.out.println("New Position is: " + pos);
-                  else{
-						pos = pos - roll;
-                  System.out.println("Remains at same position: " + pos);
-						}
-						cntr2++;
-						continue a;
-            case SNAKE:
-						System.out.println("Got SNAKE");
-                  pos = pos - roll;
-                  if (pos <= 100){
-                  if (pos >= 0 ){
-                  System.out.println("New Position is: " + pos);
-						}
-                  else{
-                  pos = 0;
-                  System.out.println("New Position is: " + pos);
-                  }
-						}
-                  else{
-                  System.out.println("New position is:" + pos);
-						}
-						cntr3++;
-                  break;
-            default:
-                  System.out.println("Default");
-                  break;
+      		int roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		cntForBets++;
+     		 System.out.println("Number on rolled dice is : " +roll);
+     		 conition1 = (int) (Math.floor(Math.random() * 10) % 3);
+      		 System.out.println("Condition: " +condition1);
+     		 switch (condition1){
+           	 case NO_PLAY:
+			System.out.println("NO PLAY");
+                 	System.out.println("New Position is: " + positionPlayer1);
+			cntForNoPlay++;
+                 	 break;
+           	 case LADDER:
+			  System.out.println("Got LADDER");
+			  positionPlayer1 = positionPlayer1 + roll;
+			  if(positionPlayer1 <= 100)
+			  System.out.println("New Position is: " + positionPlayer1);
+			  else{
+			  positionPlayer1 = positionPlayer1 - roll;
+			  System.out.println("Remains at same position: " + positionPlayer1);
+			  }
+			  cntForLadder++;
+			  continue a;
+		 case SNAKE:
+			  System.out.println("Got SNAKE");
+			  positionPlayer1 = positionPlayer1 - roll;
+			  if (positionPlayer1 >= 0 ){
+			  System.out.println("New Position is: " + positionPlayer1);
+			  }
+			  else{
+			  positionPlayer1 = 0;
+			  System.out.println("New Position is: " + positionPlayer1);
+			  }
+		          cntForSnake++;
+			  break;
+            	default:
+                  	System.out.println("Default");
+                  	break;
          }
 		}
-		int con1 = 1;
-b:		while(con1 == 1 && pos1 < 100){
+		int condition2 = 1;
+b:		while(condition2 == 1 && positionPlayer2 < 100){
 		System.out.println("PLAYER 2");
 		int roll1 = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-      c1++;
-      System.out.println("Number on rolled dice is : " +roll1);
-      con1 = (int) (Math.floor(Math.random() * 10) % 3);
-      System.out.println("Condition: " +con1);
-      switch (con1){
-            case NO_PLAY:
-						System.out.println("NO PLAY");
-                  System.out.println("New Position is: " + pos1);
-                  c4++;
-                  break;
-            case LADDER:
-						System.out.println("Got LADDER");
-                  pos1 = pos1 + roll1;
-                  if(pos1 <= 100)
-                  System.out.println("New Position is: " + pos1);
-                  else{
-                  pos1 = pos1 - roll1;
-                  System.out.println("Remains at same position: " + pos1);
-                  }
-                  c2++;
-						continue b;
-            case SNAKE:
-						System.out.println("Got SNAKE");
-                  pos1 = pos1 - roll1;
-                  if (pos1 <= 100){
-                  if (pos1 >= 0 ){
-                  System.out.println("New Position is: " + pos1);
-                  }
-                  else{
-                  pos1 = 0;
-                  System.out.println("New Position is: " + pos1);
-                  }
-                  }
-                  else{
-                  System.out.println("New position is:" + pos1);
-                  }
-                  c3++;
-                  break;
-            default:
-                  System.out.println("Default");
-                  break;
-       	  }
+		cntForBets2++;
+	      	System.out.println("Number on rolled dice is : " +roll1);
+	      	condition2 = (int) (Math.floor(Math.random() * 10) % 3);
+	      	System.out.println("Condition: " +condition2);
+	      	switch (condition2){
+           	 	case NO_PLAY:
+				 System.out.println("NO PLAY");
+                 		 System.out.println("New Position is: " + position2);
+                 		 cntForNoPlay++;
+                 	 break;
+           		 case LADDER:
+				  System.out.println("Got LADDER");
+				  position2 = position2 + roll1;
+				  if(position2 <= 100)
+				  System.out.println("New Position is: " + position2);
+				  else{
+				  position2 = position2 - roll1;
+				  System.out.println("Remains at same position: " + position2);
+				  }
+				  cntForLadder++;
+				  continue b;
+           		 case SNAKE:
+				  System.out.println("Got SNAKE");
+				  position2 = position2 - roll1;
+				  if (position2 >= 0 ){
+				  System.out.println("New Position is: " + position2);
+				  }
+				  else{
+				  position2 = 0;
+				  System.out.println("New Position is: " + position2);
+				  }
+				  cntForSnake++;
+				  break;
+			 default:
+				  System.out.println("Default");
+				  break;
+       	  			}
 			}
 		}
 
       System.out.println();
-		System.out.println("-------------------");
-      if (pos == 100){
-      System.out.println("Player1 ROLLED DICE "+ cntr1 +" time and got SNAKE "+ cntr3 +" times and got LADDER "+ cntr2 +" times and got NO PLAY "+ cntr4 +" times to win the game");
+      System.out.println("-------------------");
+      if (position1 == 100){
+      System.out.println("Player1 ROLLED DICE "+ cntForBets +" time and got SNAKE "+ cntForSnake +" times and got LADDER "+ cntForLadder +" times and got NO PLAY "+ cntForNoPlay +" times to win the game");
       System.out.println("    PLAYER1 WON");
-		}
-		else{
-      System.out.println("Player2 ROLLED DICE "+ c1 +" time and got SNAKE "+ c3 +" times and got LADDER "+ c2 +" times and got NO PLAY "+c4+" times to win the game");
-		System.out.println("    PLAYER2 WON");
-		}
+      }
+      else{
+      System.out.println("Player2 ROLLED DICE "+ cntForBets2 +" time and got SNAKE "+ cntForSnake2 +" times and got LADDER "+ cntForLadder2 +" times and got NO PLAY "+cntForNoPlay2+" times to win the game");
+      System.out.println("    PLAYER2 WON");
+      }
       System.out.println("-------------------");
 
       }
